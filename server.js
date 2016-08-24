4d988dd0674b7bc39f14042cc769d4ef89c466ff
never615 <583531034@qq.com>
2016-08-24 15:44:48 +0800

    修改配置文件

--- server.js
+++ server.js
@@ -5,14 +5,40 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+var port=3000;
+
+
+//Setting for dev server
+
+var reactHostLoader = {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loaders: ['react-hot']
+};
+
+config.module.loaders.unshift(reactHostLoader);
+
+config.plugins=config.plugins.concat([
+    new webpack.HotModuleReplacementPlugin()
+]);
+
+
+var newEntry = [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server' // "only" prevents reload on syntax errors
+];
+
+config.entry = newEntry.concat(config.entry.app);
+
+
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, 'localhost', function (err, result) {
     if (err) {
         return console.log(err);
     }
 
-    console.log('Listening at http://localhost:3000/');
+    console.log('Listening at http://localhost:'+port);
 });
--- webpack.config2.js
+++ webpack.config2.js
@@ -5,22 +5,31 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './app/app.js',
-    },
+
+
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        app = './app/app.js'
+    ],
 
     output: {
+
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         publicPath: '/build/'
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin()
     ],
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
+                loaders: ['react-hot']
+            },
+            {
                 loader: 'babel',
                 query: {
                     presets: ['es2015', 'react']
